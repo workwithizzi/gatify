dddb633ec3cfd7d663684ef39b42a45072aea330
IvaTsu <ivan.tsud@gmail.com>
2020-02-13 14:50:46 +0200

    Fix 'warn' with 'case-sensitivity'

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import SEO from "../components/SEO"
 
 const AboutPage = ({location}) => (
 	<Layout location={location}>
