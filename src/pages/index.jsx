b2dd6332d4bcc2c3d40fde28ce5c55d6eecf0a96
IvaTsu <ivan.tsud@gmail.com>
2020-02-13 14:39:16 +0200

    Rename 'Seo.jsx' -> 'SEO.jsx'

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import SEO from "../components/SEO"
 import Listing from "../components/Listing"
 
 const IndexPage = ({location}) => (
